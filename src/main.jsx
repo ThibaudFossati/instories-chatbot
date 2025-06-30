19ddc334eeccf6e37a5183f41dc44d229a21e2ba
ThibaudFossati <tfossati@gmail.com>
2025-06-30 23:26:12 +0200

    fix(build): remove missing import of styles/theme.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/theme.css'
 import './index.css'
 import App from './App.jsx'
 
