cf064638e01263da4ec3622522e3cebe63da3198
ThibaudFossati <tfossati@gmail.com>
2025-07-02 13:15:42 +0200

    ui: avatar, responsive container, contact circle button

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,4 @@
-import React from 'react';
 import './Loader.css';
-
 export default function Loader() {
-  return (
-    <div className="loader">
-      <span className="dot" />
-      <span className="dot" />
-      <span className="dot" />
-    </div>
-  );
+  return <div className="loader"><span/><span/><span/></div>;
 }
